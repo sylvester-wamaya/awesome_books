3c77ccc887957d331fb7fabb93f9405d289392f7
diarisdiakite <diarisdiakite@gmail.com>
2023-05-01 19:15:15 +0200

    Add linters. Add Objects' list

---
+++ assets/js/list.js
@@ -0,0 +1,14 @@
+const books = [
+  {
+    title: 'title1',
+    author: 'Didy',
+  },
+  {
+    title: 'title1',
+    author: 'Sylvester',
+  },
+  {
+    title: 'title1',
+    author: 'Raphael',
+  },
+];
