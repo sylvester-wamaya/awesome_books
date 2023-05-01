34812c72856d703304c9742ca8a01a566a2afc18
3nityGitHub <60592651+3nityGitHub@users.noreply.github.com>
2023-05-01 19:03:42 +0100

    Add  addBook function to list.js

--- assets/js/list.js
+++ assets/js/list.js
@@ -1,14 +1,25 @@
 const books = [
   {
+    id: 1,
     title: 'title1',
     author: 'Didy',
   },
   {
+    id: 2,
     title: 'title1',
     author: 'Sylvester',
   },
   {
+    id: 3,
     title: 'title1',
     author: 'Raphael',
   },
 ];
+
+function addBooks(title, author){
+  const book = { title: title, author: author}
+  books.push(book);
+}
+
+addBooks('book4', 'author');
+console.log(books)
