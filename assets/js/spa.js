86ce3d6459e3838722a4cc3cb08cb0cc8aabf578
Sylvester Wamaya <sylvesterwamaya@gmail.com>
2023-05-05 04:19:09 +0300

    Add navigation displays to the page and update on styling

--- assets/js/spa.js
+++ assets/js/spa.js
@@ -1,20 +1,2 @@
 //write a display page function
 // use addEventListeners to the pages links
-let displayBooks = document.getElementById('display-div');
-let navbarDisplaylink = getElementById('display-list');
-let displayAddBook = document.getElementById('display-div');
-let navbarAddlink = getElementById('display-add');
-let displayContact = document.getElementById('contact-div');
-let navbarContactlink = getElementById('display-contact');
-  
-navbarDisplaylink.addEventListener('click', () => {
-  displayBooks.classList.remove('hidden');
-})
-
-navbarAddlink.addEventListener('click', () => {
-  displayAddBook.classList.remove('hidden');
-})
-
-navbarContactlink.addEventListener('click', () => {
-  displayContact.classList.remove('hidden');
-})
